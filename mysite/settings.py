380166d4ad59bf19f98cffc667ef0bdb8d72f393
django-iori <iori4321999@gmail.com>
2023-10-01 19:48:27 +0900

    new feature

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import boto3
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,48 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #collectstatic先のディレクトリ（本番環境）
 STATIC_ROOT = '/usr/share/nginx/html/static'
 MEDIA_ROOT = '/usr/share/nginx/html/media'
+
+# Logginの設定（CloudWatchに出力）
+AWS_REGION_NAME = "us-west-2"
+
+boto3_logs_client = boto3.client("logs", region_name=AWS_REGION_NAME)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'DEBUG',
+        # Adding the watchtower handler here causes all loggers in the project that
+        # have propagate=True (the default) to send messages to watchtower. If you
+        # wish to send only from specific loggers instead, remove "watchtower" here
+        # and configure individual loggers below.
+        'handlers': ['watchtower', 'console'],
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'watchtower': {
+            'class': 'watchtower.CloudWatchLogHandler',
+            'boto3_client': boto3_logs_client,
+            'log_group_name': 'YOUR_DJANGO_PROJECT_NAME',
+            # Decrease the verbosity level here to send only those logs to watchtower,
+            # but still see more verbose logs in the console. See the watchtower
+            # documentation for other parameters that can be set here.
+            'level': 'DEBUG'
+        }
+    },
+    'loggers': {
+        # In the debug server (`manage.py runserver`), several Django system loggers cause
+        # deadlocks when using threading in the logging handler, and are not supported by
+        # watchtower. This limitation does not apply when running on production WSGI servers
+        # (gunicorn, uwsgi, etc.), so we recommend that you set `propagate=True` below in your
+        # production-specific Django settings file to receive Django system logs in CloudWatch.
+        'django': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False
+        }
+        # Add any other logger-specific configuration here.
+    }
+}
